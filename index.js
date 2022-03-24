9193a4bafa4bfa7b2d6517b530d660fe9170d424
SneakyWolfy <64506745+SneakyWolfy@users.noreply.github.com>
2022-03-24 03:09:52 -0230

    Added linter and basic file strucuter

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 //? Everything in ".env" will be stored into "process.env"
 require("dotenv").config();
-
-import { Client } from "discord.js";
-const client = Client;
-
-const token = process.env.BOT_TOKEN;
