153f494eae4d3328acf7efa167cc1a6851a1f78a
trungu <tn.nguyencs07@gmail.com>
2022-03-26 18:04:00 -0500

    Created new purge command

---
+++ src/commands/Purge.js
@@ -0,0 +1,31 @@
+const Command = require("../utils/Command");
+
+class Purge extends Command {
+  constructor() {
+    super({
+      description:
+        "Deletes a specified count of messages in a channel (Limit 100)",
+    });
+    this.data.addIntegerOption((option) =>
+      option
+        .setName("amount")
+        .setDescription("The amount of messages to purge")
+        .setRequired(true)
+    );
+  }
+
+  async action(interaction) {
+    const amount = await interaction.options.getInteger("amount");
+
+    const messages = await interaction.channel.messages.fetch({
+      limit: amount > 100 ? 100 : amount,
+    });
+
+    await messages.forEach((m) => m.delete());
+    await interaction.reply(
+      `Successfully purged ${Array.from(messages).length} messages`
+    );
+  }
+}
+
+module.exports = new Purge();
