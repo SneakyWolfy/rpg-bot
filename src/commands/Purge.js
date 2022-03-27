2ecd226db6d38f22b15c5685ff19a53be477a53e
trungu <tn.nguyencs07@gmail.com>
2022-03-26 19:17:13 -0500

    Changed purge command

--- src/commands/Purge.js
+++ src/commands/Purge.js
@@ -12,16 +12,26 @@ class Purge extends Command {
         .setDescription("The amount of messages to purge")
         .setRequired(true)
     );
+    this.data.addUserOption((option) =>
+      option.setName("user").setDescription("The targetted user to purge from")
+    );
   }
 
   async action(interaction) {
     const amount = await interaction.options.getInteger("amount");
+    const user = await interaction.options.getUser("user");
+
+    console.log(user);
 
     const messages = await interaction.channel.messages.fetch({
       limit: amount > 100 ? 100 : amount,
     });
 
-    await messages.forEach((m) => m.delete());
+    await messages.filter((m) => {
+      if (user) return m.author === user;
+      return m;
+    });
+    interaction.channel.bulkDelete(messages);
     await interaction.reply(
       `Successfully purged ${Array.from(messages).length} messages`
     );
